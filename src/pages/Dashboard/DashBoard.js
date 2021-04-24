d4d42e8a44d421477a9566a7abd9a606eb4e30ff
Vedant Singh <67269716+imvsr-69@users.noreply.github.com>
2021-04-24 16:52:33 +0530

    dashboard updated

--- src/pages/Dashboard/DashBoard.js
+++ src/pages/Dashboard/DashBoard.js
@@ -1,28 +1,28 @@
-import { useContext } from "react";
-import SideBar from "../../components/SideBar/SideBar";
-import LiveClasses from "../../components/LiveClasses/LiveClasses";
-import Calendar from "../../components/Calendar/Calendar";
-import Courses from "../../components/Courses/Courses";
-import Task from "../../components/Task/Task";
-import UserContext from "../../context/authContext";
-import PastQuiz from "../../components/PastQuiz/PastQuiz";
-import "./DashBoard.css";
+import { useContext } from 'react';
+import SideBar from '../../components/SideBar/SideBar';
+import LiveClasses from '../../components/LiveClasses/LiveClasses';
+import Calendar from '../../components/Calendar/Calendar';
+import Courses from '../../components/Courses/Courses';
+import Task from '../../components/Task/Task';
+import UserContext from '../../context/authContext';
+import PastQuiz from '../../components/PastQuiz/PastQuiz';
+import './DashBoard.css';
 
 const DashBoard = () => {
-  const { userDetails } = useContext(UserContext);
+	const { userDetails } = useContext(UserContext);
 
-  return (
-    <div className="Dashboard">
-      <SideBar active="dashboard" />
-      <div className="dashboard-items">
-        <LiveClasses user={userDetails} />
-        <PastQuiz user={userDetails} />
-        <Calendar />
-        <Task user={userDetails} />
-        <Courses user={userDetails} />
-      </div>
-    </div>
-  );
+	return (
+		<div className="Dashboard">
+			<SideBar active="dashboard" />
+			<div className="dashboard-items">
+				<LiveClasses user={userDetails} />
+				<Courses user={userDetails} />
+				<Calendar />
+				<PastQuiz user={userDetails} />
+				<Task user={userDetails} />
+			</div>
+		</div>
+	);
 };
 
 export default DashBoard;
