c39a219ebfa67e68c81204418e8b00befac0e786
Vedant Singh <67269716+imvsr-69@users.noreply.github.com>
2021-04-29 23:10:06 +0530

    coursepage completed

--- src/components/CourseContent/SingleLessonContent.js
+++ src/components/CourseContent/SingleLessonContent.js
@@ -1,89 +1,111 @@
-import { useState, useEffect, useRef } from "react";
-import SingleLessonEdit from "./SingleLessonEdit";
-import { VscFilePdf } from "react-icons/vsc";
-import { AiOutlineFileText } from "react-icons/ai";
-import { BiTask } from "react-icons/bi";
-import { MdAssignment } from "react-icons/md";
-import { IoIosPlayCircle } from "react-icons/io";
-import { FaHandMiddleFinger } from "react-icons/fa";
-import { AiOutlineDelete } from "react-icons/ai";
-import { MdEdit } from "react-icons/md";
-import axios from "../../axios/axios";
-import "./UniqueCourse.css";
+import { useState, useEffect, useRef } from 'react';
+import SingleLessonEdit from './SingleLessonEdit';
+import { VscFilePdf } from 'react-icons/vsc';
+import { AiOutlineFileText } from 'react-icons/ai';
+import { BiTask } from 'react-icons/bi';
+import { MdAssignment } from 'react-icons/md';
+import { IoIosPlayCircle } from 'react-icons/io';
+import { FaHandMiddleFinger } from 'react-icons/fa';
+import { AiOutlineDelete } from 'react-icons/ai';
+import { MdEdit } from 'react-icons/md';
+import axios from '../../axios/axios';
+import './UniqueCourse.css';
 
 const SingleLessonContent = ({
-  singleContent,
-  user,
-  id,
-  index,
-  lessonId,
-  fetchLessonContent,
+	singleContent,
+	user,
+	id,
+	index,
+	lessonId,
+	fetchLessonContent,
 }) => {
-  const clickRef = useRef(null);
-  const [showLessonEdit, setShowLessonEdit] = useState(false);
-  // console.log(singleContent);
+	const clickRef = useRef(null);
+	const [showLessonEdit, setShowLessonEdit] = useState(false);
+	const [showDelete, setShowDelete] = useState(false);
+	// console.log(singleContent);
 
-  const deleteLessonContent = async () => {
-    try {
-      const config = {
-        headers: { Authorization: `Bearer ${user.access}` },
-      };
-      const { data } = await axios.delete(
-        `/api/editLessonContent/${id}`,
-        config
-      );
-      fetchLessonContent();
-      console.log(data);
-    } catch (err) {
-      console.log(err.message);
-    }
-  };
+	const deleteLessonContent = async () => {
+		try {
+			const config = {
+				headers: { Authorization: `Bearer ${user.access}` },
+			};
+			const { data } = await axios.delete(
+				`/teacher/editLessonContent/${id}`,
+				config,
+			);
+			fetchLessonContent();
+			setShowDelete(false);
+			console.log(data);
+		} catch (err) {
+			console.log(err.message);
+		}
+	};
 
-  const toggleIcon = () => {
-    switch (singleContent?.media_type) {
-      case "text":
-        return <AiOutlineFileText />;
-      case "quiz":
-        return <MdAssignment />;
-      case "video":
-        return <IoIosPlayCircle />;
-      case "homework":
-        return <BiTask />;
-      case "pdf":
-        return <VscFilePdf />;
-      case "assignment":
-        return <MdAssignment />;
-      default:
-        return <FaHandMiddleFinger />;
-    }
-  };
+	const toggleIcon = () => {
+		switch (singleContent?.media_type) {
+			case 'text':
+				return <AiOutlineFileText />;
+			case 'quiz':
+				return <MdAssignment />;
+			case 'video':
+				return <IoIosPlayCircle />;
+			case 'homework':
+				return <BiTask />;
+			case 'pdf':
+				return <VscFilePdf />;
+			case 'assignment':
+				return <MdAssignment />;
+			default:
+				return <FaHandMiddleFinger />;
+		}
+	};
 
-  return (
-    <div ref={clickRef} className="single-lesson-content">
-      <div className="lesson-left">
-        <div className="label">
-          <div>{singleContent?.media_type}</div>
-        </div>
-      </div>
-      <div className="single-lesson-icons">
-        <button>{toggleIcon()}</button>
-        <button onClick={deleteLessonContent}>
-          <AiOutlineDelete />
-        </button>
-        <button onClick={() => setShowLessonEdit(true)}>
-          <MdEdit />
-        </button>
-      </div>
-      {showLessonEdit && (
-        <SingleLessonEdit
-          setShowLessonEdit={setShowLessonEdit}
-          mediaType={singleContent?.media_type}
-          user={user}
-          id={id}
-        />
-      )}
-    </div>
-  );
+	return (
+		<div ref={clickRef} className="single-lesson-content">
+			<div className="lesson-left">
+				<div className="label">
+					<div>{singleContent?.media_type}</div>
+					<div>{singleContent?.text}</div>
+					<div>{singleContent?.quiz_name}</div>
+					<div>{singleContent?.assignment_name}</div>
+					<div>{singleContent?.descripion}</div>
+					<div>{singleContent?.text_content}</div>
+				</div>
+			</div>
+			<div className="single-lesson-icons">
+				<button>{toggleIcon()}</button>
+				<button onClick={() => setShowDelete(true)}>
+					<AiOutlineDelete />
+				</button>
+				<button onClick={() => setShowLessonEdit(true)}>
+					<MdEdit />
+				</button>
+			</div>
+			{showDelete && (
+				<div className="delete-modal-wrapper">
+					<div className="delete-modal">
+						<p>Are you sure you want to delete your course ?</p>
+						<div className="lesson-delete-modal-btn">
+							<button onClick={() => setShowDelete(false)}>Cancel</button>
+							<button onClick={deleteLessonContent}>Proceed and Delete</button>
+						</div>
+					</div>
+				</div>
+			)}
+			{showLessonEdit && (
+				<SingleLessonEdit
+					setShowLessonEdit={setShowLessonEdit}
+					mediaType={singleContent?.media_type}
+					description={singleContent?.descripion}
+					text_content={singleContent?.text}
+					url={singleContent?.link}
+					user={user}
+					id={id}
+					fetchLessonContent={fetchLessonContent}
+				/>
+			)}
+		</div>
+	);
 };
 
 export default SingleLessonContent;
--- src/components/CourseContent/SingleLessonEdit.js
+++ src/components/CourseContent/SingleLessonEdit.js
@@ -1,185 +1,149 @@
-import { useEffect, useRef, useState } from "react";
-import axios from "../../axios/axios";
-import "./SingleLessonEdit.css";
+import { useEffect, useRef, useState } from 'react';
+import axios from '../../axios/axios';
+import './SingleLessonEdit.css';
 
-const ScheduleClass = ({ setShowLessonEdit, mediaType, user, id }) => {
-  const [textContent, setTextContent] = useState("");
-  const [videoUrl, setVideoUrl] = useState("");
-  const [videoDescription, setVideoDescription] = useState("");
-  const [pdfFile, setPdfFile] = useState(null);
-  const [quizName, setQuizName] = useState("");
-  const [duration, setDuration] = useState("");
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
+const ScheduleClass = ({
+	setShowLessonEdit,
+	mediaType,
+	user,
+	id,
+	description,
+	text_content,
+	url,
+	fetchLessonContent,
+}) => {
+	const [textContent, setTextContent] = useState(text_content);
+	const [videoUrl, setVideoUrl] = useState(url);
+	const [videoDescription, setVideoDescription] = useState(description);
+	const [pdfFile, setPdfFile] = useState(null);
 
-  const modalRef = useRef(null);
+	const modalRef = useRef(null);
 
-  const editLesson = async (postData) => {
-    try {
-      const config = {
-        headers: { Authorization: `Bearer ${user.access}` },
-      };
-      const { data } = await axios.patch(
-        `/api/editLessonContent/${id}`,
-        postData,
-        config
-      );
-      console.log(data);
-    } catch (err) {
-      console.log(err.message);
-    }
-  };
+	const editLesson = async (postData) => {
+		try {
+			const config = {
+				headers: { Authorization: `Bearer ${user.access}` },
+			};
+			const { data } = await axios.patch(
+				`/teacher/editLessonContent/${id}`,
+				postData,
+				config,
+			);
+			console.log(data);
+			fetchLessonContent();
+			setShowLessonEdit(false);
+		} catch (err) {
+			console.log(err.message);
+		}
+	};
 
-  const editCourseContent = () => {
-    if (mediaType === "video") {
-      const videoData = {
-        media_type: mediaType,
-        lesson: id,
-        link: videoUrl,
-        link_desc: videoDescription,
-      };
-      console.log(videoData);
-      editLesson(videoData);
-    } else if (mediaType === "text" || mediaType === "homework") {
-      const textData = {
-        media_type: mediaType,
-        lesson: id,
-        text_content: textContent,
-      };
-      console.log(textData);
-      editLesson(textData);
-    } else if (mediaType === "pdf") {
-      let formData = new FormData();
-      formData.append("media_type", mediaType);
-      formData.append("lesson", id);
-      formData.append("pdf", pdfFile);
-      editLesson(formData);
-    } else if (mediaType === "quiz" || mediaType === "assignment") {
-      const quizData = {
-        lesson: id,
-        media_type: mediaType,
-        quiz_name: quizName,
-        duration: duration,
-        start_date: startDate,
-        expire_date: endDate,
-      };
-      editLesson(quizData);
-    }
-  };
+	const editCourseContent = () => {
+		if (mediaType === 'video') {
+			const videoData = {
+				link: videoUrl,
+				link_desc: videoDescription,
+			};
+			console.log(videoData);
+			editLesson(videoData);
+		} else if (mediaType === 'text' || mediaType === 'homework') {
+			const textData = {
+				text_content: textContent,
+			};
+			console.log(textData);
+			editLesson(textData);
+		} else if (mediaType === 'pdf') {
+			let formData = new FormData();
 
-  useEffect(() => {
-    const handler = (e) => {
-      if (!modalRef.current?.contains(e.target)) {
-        setShowLessonEdit(false);
-      }
-    };
-    window.addEventListener("click", handler);
-    return () => window.removeEventListener("click", handler);
-  });
+			formData.append('pdf', pdfFile);
+			editLesson(formData);
+		}
+	};
 
-  return (
-    <div className="single-lesson-edit-modal">
-      <div className="single-lesson-edit-card" ref={modalRef}>
-        <h1>Edit {mediaType}</h1>
+	useEffect(() => {
+		const handler = (e) => {
+			if (!modalRef.current?.contains(e.target)) {
+				setShowLessonEdit(false);
+			}
+		};
+		window.addEventListener('click', handler);
+		return () => window.removeEventListener('click', handler);
+	});
 
-        {mediaType === "text" || mediaType === "homework" ? (
-          <label>
-            <p>Enter Text Content</p>
-            <textarea
-              name="text_coontent"
-              value={textContent}
-              onChange={(e) => setTextContent(e.target.value)}
-            />
-          </label>
-        ) : (
-          <></>
-        )}
-        {mediaType === "quiz" || mediaType === "assignment" ? (
-          <div className="quiz-content">
-            <label>
-              <p>Enter Content Name</p>
-              <input
-                type="text"
-                value={quizName}
-                onChange={(e) => setQuizName(e.target.value)}
-              />
-            </label>
-            <label>
-              <p>Enter Duration</p>
-              <input
-                type="text"
-                value={duration}
-                onChange={(e) => setDuration(e.target.value)}
-              />
-            </label>
-            <label>
-              <p>Enter Start Date</p>
-              <input
-                type="datetime-local"
-                value={startDate}
-                onChange={(e) => setStartDate(e.target.value)}
-              />
-            </label>
-            <label>
-              <p>Enter End Date</p>
-              <input
-                type="datetime-local"
-                value={endDate}
-                onChange={(e) => setEndDate(e.target.value)}
-              />
-            </label>
-          </div>
-        ) : (
-          <></>
-        )}
+	return (
+		<div className="single-lesson-edit-modal">
+			<div className="single-lesson-edit-card" ref={modalRef}>
+				<h1>Edit {mediaType}</h1>
 
-        {mediaType === "pdf" && (
-          <label>
-            <p>Upload Document</p>
-            <input
-              type="file"
-              onChange={(e) => setPdfFile(e.target.files[0])}
-            />
-          </label>
-        )}
+				{mediaType === 'text' || mediaType === 'homework' ? (
+					<label>
+						<p>Enter Text Content</p>
+						<textarea
+							name="text_coontent"
+							value={textContent}
+							onChange={(e) => setTextContent(e.target.value)}
+						/>
+					</label>
+				) : (
+					<></>
+				)}
+				{mediaType === 'quiz' || mediaType === 'assignment' ? (
+					<h4>Edit these content in Myquiz Section</h4>
+				) : (
+					<></>
+				)}
 
-        {mediaType === "video" && (
-          <div
-            className="video-content"
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              height: " 150px",
-              justifyContent: "space-around",
-            }}
-          >
-            <label>
-              <p>Enter video url</p>
-              <input
-                type="text"
-                name="video_url"
-                value={videoUrl}
-                onChange={(e) => setVideoUrl(e.target.value)}
-              />
-            </label>
-            <label>
-              <p>Enter video Description</p>
-              <textarea
-                name="video_desc"
-                value={videoDescription}
-                onChange={(e) => setVideoDescription(e.target.value)}
-              />
-            </label>
-          </div>
-        )}
+				{mediaType === 'pdf' && (
+					<label>
+						<p>Upload Document</p>
+						<input
+							type="file"
+							onChange={(e) => setPdfFile(e.target.files[0])}
+						/>
+					</label>
+				)}
 
-        <div className="modal-btn">
-          <button onClick={editCourseContent}>Create Lesson Content</button>
-          <button onClick={() => setShowLessonEdit(false)}>Cancel</button>
-        </div>
-      </div>
-    </div>
-  );
+				{mediaType === 'video' && (
+					<div
+						className="video-content"
+						style={{
+							display: 'flex',
+							flexDirection: 'column',
+							height: ' 150px',
+							justifyContent: 'space-around',
+						}}
+					>
+						<label>
+							<p>Enter video url</p>
+							<input
+								type="text"
+								name="video_url"
+								value={videoUrl}
+								onChange={(e) => setVideoUrl(e.target.value)}
+							/>
+						</label>
+						<label>
+							<p>Enter video Description</p>
+							<textarea
+								name="video_desc"
+								value={videoDescription}
+								onChange={(e) => setVideoDescription(e.target.value)}
+							/>
+						</label>
+					</div>
+				)}
+
+				<div className="modal-btn">
+					<button
+						onClick={editCourseContent}
+						disabled={mediaType === 'quiz' || mediaType === 'assignment'}
+					>
+						Edit Lesson Content
+					</button>
+					<button onClick={() => setShowLessonEdit(false)}>Cancel</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ScheduleClass;
