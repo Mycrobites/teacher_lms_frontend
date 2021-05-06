84ef8307bdb50e77069f6c00023aa616e1a0e1ff
paramkeshkamat <keshkamatparam@gmail.com>
2021-05-06 15:30:59 +0530

    filters updated

--- src/pages/QuizEditPage/QuizEditPage.js
+++ src/pages/QuizEditPage/QuizEditPage.js
@@ -17,6 +17,11 @@ const QuizEditPage = () => {
   //skills
   const [skills, setSkills] = useState(null);
   const [skillLevel, setSkillLevel] = useState("");
+  //subject
+  const [subjects, setSubjects] = useState(null);
+  const [selectedSubject, setSelectedSubject] = useState("");
+  const [selectedTopic, setSelectedTopic] = useState("");
+  const [selectedSubTopics, setSelectedSubTopics] = useState("");
   //others
   const [selectedQuestions, setSelectedQuestions] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -26,6 +31,12 @@ const QuizEditPage = () => {
   //react-router
   const { id } = useParams();
   const history = useHistory();
+
+  console.log(difficultyLevel);
+  console.log(skillLevel);
+  console.log(selectedSubject);
+  console.log(selectedTopic);
+  console.log(selectedSubTopics);
 
   const handleChange = (qid) => {
     if (selectedQuestions.includes(qid)) {
@@ -62,13 +73,24 @@ const QuizEditPage = () => {
   };
 
   const filterQuestions = () => {
-    setFilteredQuestionBank(
-      questionBank.filter(
-        (ques) =>
-          ques.dificulty_tag === difficultyLevel && ques.skill === skillLevel
-      )
-    );
-    console.log(filteredQuestionBank);
+    if (!skillLevel && !difficultyLevel) {
+      setFilteredQuestionBank(questionBank);
+    } else if (!skillLevel && difficultyLevel.length > 0) {
+      setFilteredQuestionBank(
+        questionBank.filter((ques) => ques.dificulty_tag === difficultyLevel)
+      );
+    } else if (!difficultyLevel && skillLevel.length > 0) {
+      setFilteredQuestionBank(
+        questionBank.filter((ques) => ques.skill === skillLevel)
+      );
+    } else {
+      setFilteredQuestionBank(
+        questionBank.filter(
+          (ques) =>
+            ques.dificulty_tag === difficultyLevel && ques.skill === skillLevel
+        )
+      );
+    }
   };
 
   const clearFilters = () => {
@@ -90,7 +112,9 @@ const QuizEditPage = () => {
         //difficulty
         setDifficulty(data.tags.dificulty);
         //skills
-        setSkills(["Calculation", "Conceptual"]);
+        setSkills(data.tags.skill);
+        //subjects
+        setSubjects(data.tags.subject);
       } catch (err) {
         console.log(err.message);
       }
@@ -98,9 +122,6 @@ const QuizEditPage = () => {
     getQuestionBank();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  console.log("f", filteredQuestionBank);
-  console.log("q", questionBank);
 
   if (!questionBank) {
     return (
@@ -113,53 +134,128 @@ const QuizEditPage = () => {
   return (
     <div className="quiz-questions-div">
       <div className="questionbank-header">
+        {/* TITLE */}
         <div className="quiz-question-header">
           <h1>Question Bank</h1>
-          <button
-            disabled={selectedQuestions.length === 0}
-            onClick={addQuestions}
-          >
-            Add Questions
-          </button>
-          <button onClick={() => setShowFilter(!showFilter)}>
-            Filter <BsFilterRight />
-          </button>
+          <div className="quiz-header-buttons">
+            <button
+              disabled={selectedQuestions.length === 0}
+              onClick={addQuestions}
+            >
+              Add Questions
+            </button>
+            <button onClick={() => setShowFilter(!showFilter)}>
+              Filter <BsFilterRight />
+            </button>
+          </div>
         </div>
+
+        {/* FILTER */}
         {showFilter && (
           <div className="filter">
-            <div className="difficulty-level">
-              <label>Difficulty</label>
-              <select
-                value={difficultyLevel}
-                onChange={(e) => setDifficultyLevel(e.target.value)}
-              >
-                {difficulty.map((d, i) => (
-                  <option key={i} value={d}>
-                    {d}
-                  </option>
-                ))}
-                <option value="">All</option>
-              </select>
+            <div className="filter-selects">
+              <h2>Filter</h2>
+              <div className="difficulty-level">
+                <label>
+                  <p>Difficulty</p>
+                  <select
+                    value={difficultyLevel}
+                    onChange={(e) => setDifficultyLevel(e.target.value)}
+                  >
+                    {difficulty?.map((d, i) => (
+                      <option key={i} value={d}>
+                        {d}
+                      </option>
+                    ))}
+                    <option value="">All</option>
+                  </select>
+                </label>
+              </div>
+
+              <div className="skill-level">
+                <label>
+                  <p>Skill</p>
+                  <select
+                    value={skillLevel}
+                    onChange={(e) => setSkillLevel(e.target.value)}
+                  >
+                    {skills?.map((s, i) => (
+                      <option key={i} value={s}>
+                        {s}
+                      </option>
+                    ))}
+                    <option value="">All</option>
+                  </select>
+                </label>
+              </div>
+
+              <div className="subject">
+                <label>
+                  <p>Subject</p>
+                  <select
+                    value={selectedSubject}
+                    onChange={(e) => setSelectedSubject(e.target.value)}
+                  >
+                    {subjects?.map((s, i) => (
+                      <option key={i} value={`${i}-${s.name}`}>
+                        {s.name}
+                      </option>
+                    ))}
+                    <option value="">All</option>
+                  </select>
+                </label>
+              </div>
+
+              {selectedSubject !== "" && (
+                <div className="topics">
+                  <label>
+                    <p>Topics</p>
+                    <select
+                      value={selectedTopic}
+                      onChange={(e) => setSelectedTopic(e.target.value)}
+                    >
+                      {subjects[selectedSubject.split("-")[0]].topics?.map(
+                        (t, i) => (
+                          <option key={i} value={t.name}>
+                            {t.name}
+                          </option>
+                        )
+                      )}
+                      <option value="">All</option>
+                    </select>
+                  </label>
+                </div>
+              )}
+
+              {selectedSubject !== "" && selectedTopic !== "" && (
+                <div className="sub-topic">
+                  <label>
+                    <p>SubTopics</p>
+                    <select
+                      value={selectedSubTopics}
+                      onChange={(e) => setSelectedSubTopics(e.target.value)}
+                    >
+                      {subjects.topics?.subTopics?.map((st, i) => (
+                        <option key={i} value={st.name}>
+                          {st.name}
+                        </option>
+                      ))}
+                      <option value="">All</option>
+                    </select>
+                  </label>
+                </div>
+              )}
             </div>
-            <div className="skill-level">
-              <label>Skils</label>
-              <select
-                value={skillLevel}
-                onChange={(e) => setSkillLevel(e.target.value)}
-              >
-                {skills.map((s, i) => (
-                  <option key={i} value={s}>
-                    {s}
-                  </option>
-                ))}
-                <option value="">All</option>
-              </select>
+
+            <div className="filter-buttons">
+              <button onClick={clearFilters}>Clear filters</button>
+              <button onClick={filterQuestions}>Apply filters</button>
             </div>
-            <button onClick={clearFilters}>clear filters</button>
-            <button onClick={filterQuestions}>Okay</button>
           </div>
         )}
       </div>
+
+      {/* QUESTION BANK QUESTIONS */}
       {filteredQuestionBank?.map((ques) => (
         <div className="quiz-question" key={ques.id}>
           <div className="check-box">
@@ -185,6 +281,8 @@ const QuizEditPage = () => {
           </div>
         </div>
       ))}
+
+      {/* LOADER */}
       {loading && (
         <div className="quizquestion-loader">
           <Loader />
@@ -195,3 +293,23 @@ const QuizEditPage = () => {
 };
 
 export default QuizEditPage;
+
+/*
+if(!difficultyLevel && !skillLevel && !selectedSubject) {
+  display all
+} else if(1 && !2 && !3) {
+  filter acc to 1
+} else if(!1 && 2 && !3) {
+  filter acc to 2
+} else if(!1 && !2 && 3) {
+  filter acc to 3
+} else if(1 && 2 && !3) {
+  filter acc to 1 and 2
+} else if(1 && !2 && 3) {
+  filter acc to 1 and 3
+} else if(!1 && 2 && 3) {
+  filter acc to 2 and 3
+} else {
+  display 1 && 2 && 3
+}
+*/
